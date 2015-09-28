536c0efece36e0320fc7aff3731aa83c06f6c0f2
Clement DENIAU <cdeniau@e2r10p20.42.fr>
2015-09-28 15:06:17 +0200

    missing S init

--- computeCost.m
+++ computeCost.m
@@ -12,6 +12,7 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+S = X * theta - y;
 J = S' * S / (2 * m);
 
 % =========================================================================
