f8f2df0f468b580b60af5cfadfa8563af46f6cb4
Clement DENIAU <cdeniau@e2r10p20.42.fr>
2015-09-28 17:13:54 +0200

    print

--- ex1.m
+++ ex1.m
@@ -9,8 +9,7 @@ m = length(y); % number of training examples
 
 % Plot Data
 % Note: You have to complete the code in plotData.m
-% plotData(X, y);
-print();
+plotData(X, y);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
