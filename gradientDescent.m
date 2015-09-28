43a25cfd00b7ac85585f8d8229883ae52131bb23
Clément "care" D. <kklleemm@users.noreply.github.com>
2015-09-28 18:57:54 +0200

    Update gradientDescent.m

--- gradientDescent.m
+++ gradientDescent.m
@@ -16,13 +16,12 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-theta = theta - (alpha * (((theta' * X')' - y)' * X) / m)';
+    theta = theta - (alpha * (((theta' * X')' - y)' * X) / m)';
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
-end
-
+    end
 end
